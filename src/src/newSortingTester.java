3c30f2d384ac461d73e05d12053db25e7597fdd2
dianapvalles <dianapaolavalles@gmail.com>
2020-04-10 13:14:19 -0600

    finished test cases with justification

--- src/src/newSortingTester.java
+++ src/src/newSortingTester.java
@@ -25,7 +25,6 @@ public class newSortingTester {
         * The array passed also includes duplicated values
         * All numbers need to be swapped at least once. If it fails then one of the methods' implementation is wrong
         * TEST: PASSED
-        *
      */
 
     @Test
@@ -34,6 +33,57 @@ public class newSortingTester {
         int[] nums = {5,3,7,2,1,9,4,5};
         int[] expected = {1,2,3,4,5,5,7,9};
         ns.newSorting(nums,3);
+
+        assertArrayEquals(expected, nums);
+    }
+
+    /*
+         * The purpose of this test is to check the algorithm's performance when the values passed include negative numbers
+         * The array passed also includes duplicated values
+         * If it fails then one of the methods' implementation is wrong but I would focus more on either on mergeSortedHalves or the partition for quickSort
+         * Since those are the methods where the swaps are made
+         * TEST: PASSED
+     */
+    @Test
+    public void newSortingTest3(){
+        newSorting ns = new newSorting();
+        int[] nums = {2,-1,0,2,-3};
+        int[] expected = {-3,-1,0,2,2};
+        ns.newSorting(nums,2);
+
+        assertArrayEquals(expected, nums);
+    }
+
+    /*
+         * The purpose of this test is to check the algorithm's performance when the values passed include negative numbers, duplicated values and positive numbers
+         * Large array
+         * If it fails then one of the methods' implementation is wrong but I would focus more on either on mergeSortedHalves or the partition for quickSort
+         * Since those are the methods where the swaps are made
+         * TEST: PASSED
+     */
+    @Test
+    public void newSortingTest4(){
+        newSorting ns = new newSorting();
+        int[] nums = {-2,19,7,8,5,3,1,0,10,15,-4,-1,9,-2,2,3};
+        int[] expected = {-4,-2,-2,-1,0,1,2,3,3,5,7,8,9,10,15,19};
+        ns.newSorting(nums,4);
+
+        assertArrayEquals(expected, nums);
+    }
+
+
+    /*
+         * The purpose of this test is to check the algorithm's performance when no values are passed
+         * If it fails then one of the methods base case might be wrong
+         * EDGE CASE
+         * TEST: PASSED
+     */
+    @Test
+    public void newSortingTest5(){
+        newSorting ns = new newSorting();
+        int[] nums = {};
+        int[] expected = {};
+        ns.newSorting(nums,4);
 
         assertArrayEquals(expected, nums);
     }
